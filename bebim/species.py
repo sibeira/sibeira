03f91afd7043c076517fd7f9a96aa67775db54a2
Mátyás Aradi <20679946+matyasaradi@users.noreply.github.com>
2021-10-12 01:15:21 +0200

    #1 Add orbital constants for alkalines atoms and ions

--- bebim/species.py
+++ bebim/species.py
@@ -5,23 +5,60 @@ RYDBERG = scipy.constants.Rydberg * scipy.constants.Planck * scipy.constants.c /
 
 class OrbitalConstants:
     def __init__(self, species):
-        self.orbital_constants = orbital_constants[species]
+        self.orbital_constants = self.__get_orbital_constants(species)
+        self.fill()
+
+    def fill(self):
         if 'N' not in self.orbital_constants:
             self.orbital_constants['N'] = 1
         if 'Q' not in self.orbital_constants:
             self.orbital_constants['Q'] = 1
         if 'n' not in self.orbital_constants:
             self.orbital_constants['n'] = 1
-        if 'B' not in self.orbital_constants:
-            self.orbital_constants['B'] = RYDBERG
         if 'U' not in self.orbital_constants:
-            self.orbital_constants['U'] = RYDBERG
+            self.fill_U()
+
+    def fill_U(self):
+        self.orbital_constants['U'] = self.orbital_constants['B']
+
+    @staticmethod
+    def __get_orbital_constants(species):
+        return orbital_constants[species]
 
     def get(self, field):
         return self.orbital_constants[field]
 
 
 orbital_constants = {
-    'H': dict(B=RYDBERG, U=RYDBERG, N=1, Q=0.5668),
-    'He': dict(B=24.587, U=3*RYDBERG, N=2, Q=0.8841)
+    'H':  dict(B=RYDBERG, Q=0.5668),
+    'He': dict(B=24.587389011, U=3*RYDBERG, N=2, Q=0.8841),
+    'Li': dict(B=5.391714996),
+    'Na': dict(B=5.13907696),
+    'K':  dict(B=4.34066373),
+    'Rb': dict(B=4.1771281),
+    'Cs': dict(B=3.89390572743)
+}
+
+
+class IonOrbitalConstants(OrbitalConstants):
+    # noinspection PyMissingConstructor
+    def __init__(self, species):
+        self.orbital_constants = self.__get_orbital_constants(species)
+        self.fill()
+
+    @staticmethod
+    def __get_orbital_constants(species):
+        return ion_orbital_constants[species]
+
+    def fill_U(self):
+        self.orbital_constants['U'] = (self.orbital_constants['Z']-5.0/16.0)**2 * 2.0 * RYDBERG
+
+
+ion_orbital_constants = {
+    'He': dict(Z=2,  B=4*RYDBERG, U=4*RYDBERG, Q=0.8841),
+    'Li': dict(Z=3,  B=75.6400970, U=14.56*RYDBERG, N=2),
+    'Na': dict(Z=11, B=47.28636, N=6),
+    'K':  dict(Z=19, B=31.62500, N=6),
+    'Rb': dict(Z=37, B=27.28954, N=6),
+    'Cs': dict(Z=55, B=23.15745, N=6)
 }
--- tests/test_species.py
+++ tests/test_species.py
@@ -5,19 +5,38 @@ import unittest
 class TestSpecies(unittest.TestCase):
     def test_hydrogen(self):
         o = OrbitalConstants('H')
-        self.assertEqual(o.get('B'), RYDBERG, 'U for hydrogen')
-        self.assertEqual(o.get('U'), RYDBERG, 'U for hydrogen')
-        self.assertEqual(o.get('N'), 1, 'N for hydrogen')
-        self.assertEqual(o.get('Q'), 0.5668, 'Q for hydrogen')
-        self.assertEqual(o.get('n'), 1, 'n for hydrogen')
+        self.assertEqual(RYDBERG, o.get('B'), 'B for hydrogen')
+        self.assertEqual(RYDBERG, o.get('U'), 'U for hydrogen')
+        self.assertEqual(1, o.get('N'),  'N for hydrogen')
+        self.assertEqual(0.5668, o.get('Q'),  'Q for hydrogen')
+        self.assertEqual(1, o.get('n'), 'n for hydrogen')
 
     def test_helium(self):
         o = OrbitalConstants('He')
-        self.assertEqual(o.get('B'), 24.587, 'U for hydrogen')
-        self.assertEqual(o.get('U'), 3*RYDBERG, 'U for hydrogen')
-        self.assertEqual(o.get('N'), 2, 'N for hydrogen')
-        self.assertEqual(o.get('Q'), 0.8841, 'Q for hydrogen')
-        self.assertEqual(o.get('n'), 1, 'n for hydrogen')
+        self.assertEqual(24.587389011, o.get('B'), 'B for helium')
+        self.assertEqual(3*RYDBERG, o.get('U'), 'U for helium')
+        self.assertEqual(2, o.get('N'), 'N for helium')
+        self.assertEqual(0.8841, o.get('Q'), 'Q for helium')
+        self.assertEqual(1, o.get('n'), 'n for helium')
+
+    def test_caesium(self):
+        o = OrbitalConstants('Cs')
+        self.assertEqual(3.89390572743, o.get('B'), 'B for caesium')
+        self.assertEqual(3.89390572743, o.get('U'), 'U for caesium')
+        self.assertEqual(1, o.get('N'), 'N for caesium')
+        self.assertEqual(1, o.get('Q'), 'Q for caesium')
+        self.assertEqual(1, o.get('n'), 'n for caesium')
+
+    def test_lithium_ion(self):
+        o = IonOrbitalConstants('Li')
+        self.assertEqual(75.6400970, o.get('B'), 'B for Li+')
+        self.assertEqual(14.56*RYDBERG, o.get('U'), 'U for Li+')
+        self.assertEqual(2, o.get('N'), 'N for Li+')
+        self.assertEqual(1, o.get('Q'), 'Q for Li+')
+        self.assertEqual(1, o.get('n'), 'n for Li+')
+        self.assertEqual(3, o.get('Z'), 'Z for Li+')
+        o.fill_U()
+        self.assertAlmostEqual(2.0*7.223*RYDBERG, o.get('U'), places=1, msg='U from variation theory for Li+')
 
 
 if __name__ == '__main__':
