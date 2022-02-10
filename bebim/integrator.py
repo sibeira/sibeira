0ff51d96fa8e3576eb2ff705fd3cc5606dafdae1
Mátyás Aradi <20679946+matyasaradi@users.noreply.github.com>
2022-02-10 23:07:16 +0100

    #19 Fix integration

--- bebim/integrator.py
+++ bebim/integrator.py
@@ -4,11 +4,25 @@ import scipy.integrate
 import scipy.stats
 
 from beam import Beam
+from bebim.cross_section import CrossSection
+from tabata_ctf.cross_section import CrossSection as CXCrossSection
 
 
 class RateIntegrator:
-    def __init__(self):
+    def __init__(self, reaction_name, beam_species, beam_energy, temperature, dimension=2):
+        self.reaction_name = reaction_name
+        self.temperature = temperature
+        self.beam_species = beam_species
+        self.beam_energy = beam_energy
+        self.beam_speed = self.get_projectile_velocity()
+        self.target_mass = self.get_target_mass()
+        self.cross_section = self.get_cross_section()
         self.normalisation_factor = 1
+        self.maxwell_normalisation_factor = self.get_maxwell_normalisation_factor(temperature)
+        self.set_integrator(dimension)
+
+    def get_coefficient(self):
+        return self.integrate(self.integrand) / self.integrate(self.integrand_normalisation)
 
     @staticmethod
     def get_third_side_length(a, b, alpha):
@@ -18,76 +32,82 @@ class RateIntegrator:
     def maxwell(v):
         return scipy.stats.maxwell.pdf(v)
 
-    @staticmethod
-    def get_impact_energy(mass, velocity):
-        return 0.5 * mass * velocity ** 2 / scipy.constants.elementary_charge
+    def get_maxwell_normalisation_factor(self, temperature):
+        return numpy.sqrt(temperature * scipy.constants.elementary_charge / self.target_mass)
 
-    def get_coefficient(self):
-        return self.integrate(self.integrand) / self.integrate(self.integrand_normalisation)
+    def get_impact_energy(self, velocity):
+        return 0.5 * self.target_mass * velocity ** 2 / scipy.constants.elementary_charge
 
+    def get_projectile_velocity(self):
+        return Beam(self.beam_species, self.beam_energy).get_speed()
 
-class ElectronRateIntegrator(RateIntegrator):
-    def __init__(self, electron_temperature, cross_section):
-        super().__init__()
-        self.electron_velocity_normalisation_factor = self.get_electron_velocity_normalisation_factor(electron_temperature)
-        self.cross_section = cross_section
+    def get_target_mass(self):
+        if self.reaction_name == 'electron impact ionisation':
+            return scipy.constants.electron_mass
+        elif self.reaction_name == 'charge exchange':
+            return Beam('D', 0).get_mass()
+        else:
+            raise ValueError('The ionisation reaction is unknown: ' + self.reaction_name)
 
-    @staticmethod
-    def get_electron_velocity_normalisation_factor(electron_temperature):
-        return numpy.sqrt(electron_temperature * scipy.constants.elementary_charge / scipy.constants.electron_mass)
+    def get_cross_section(self):
+        if self.reaction_name == 'electron impact ionisation':
+            beb = CrossSection(self.beam_species)
+            return beb.get_polynomial()
+        elif self.reaction_name == 'charge exchange':
+            tabata = CXCrossSection(self.beam_species)
+            return tabata.get_polynomial()
+            #tabata_double = CXCrossSection(self.beam_species, degree='double')
+            #return lambda x: tabata.get_polynomial()(x) + 2.0 * tabata_double.get_polynomial()(x)
+        else:
+            raise ValueError('The ionisation reaction is unknown: ' + self.reaction_name)
 
-    def integrand(self, v):
-        velocity = v * self.electron_velocity_normalisation_factor
-        impact_energy = self.get_impact_energy(scipy.constants.electron_mass, velocity)
+    def set_integrator(self, dimension):
+        if dimension == 1:
+            self.integrand = self.integrand_1d
+            self.integrand_normalisation = self.integrand_normalisation_1d
+            self.integrate = self.integrate_1d
+        elif dimension == 2:
+            self.integrand = self.integrand_2d
+            self.integrand_normalisation = self.integrand_normalisation_2d
+            self.integrate = self.integrate_2d
+        else:
+            raise ValueError('Invalid integration dimension: ' + str(dimension))
+
+    def integrand_all(self, v, alpha=0, beta=0):
+        velocity = self.get_third_side_length(v * self.maxwell_normalisation_factor, self.beam_speed, alpha)
+        impact_energy = self.get_impact_energy(velocity)
         return self.maxwell(v) * velocity * self.cross_section(impact_energy)
 
+    def integrand_normalisation_all(self, v, alpha=0, beta=0):
+        return self.maxwell(v)
 
-class IonRateIntegrator(RateIntegrator):
-    def __init__(self, electron_temperature, ion_velocity, cross_section, cross_section_of_double_ionisation=lambda x: 0):
-        self.deuterium_mass = self.get_deuterium_mass()
-        self.ion_velocity = ion_velocity
-        self.ion_velocity_normalisation_factor = self.get_ion_velocity_normalisation_factor(electron_temperature)
-        self.cross_section = cross_section
-        self.cross_section_of_double_ionisation = cross_section_of_double_ionisation
-        super().__init__()
+    def integrand_1d(self, v):
+        velocity = v * self.maxwell_normalisation_factor
+        impact_energy = self.get_impact_energy(velocity)
+        return self.maxwell(v) * velocity * self.cross_section(impact_energy)
 
     @staticmethod
-    def get_deuterium_mass():
-        return Beam('D', 0).get_mass()
-
-    def get_ion_velocity_normalisation_factor(self, electron_temperature):
-        return numpy.sqrt(electron_temperature * scipy.constants.elementary_charge / self.deuterium_mass)
-
-    def integrand(self, alpha, v):
-        velocity = self.get_third_side_length(v * self.ion_velocity_normalisation_factor, self.ion_velocity, alpha)
-        impact_energy = self.get_impact_energy(self.deuterium_mass, velocity)
-        return self.maxwell(v) * velocity * \
-            (self.cross_section(impact_energy) +
-             2.0 * self.cross_section_of_double_ionisation(impact_energy))
-
-
-class ElectronRateIntegrator1D(ElectronRateIntegrator):
-    @staticmethod
-    def integrate(function):
+    def integrate_1d(function):
         return scipy.integrate.quad(function, 0, numpy.inf)[0]
 
-    def integrand_normalisation(self, v):
+    def integrand_normalisation_1d(self, v):
         return self.maxwell(v)
 
+    def integrand_2d(self, alpha, v):
+        velocity = self.get_third_side_length(v * self.maxwell_normalisation_factor, self.beam_speed, alpha)
+        impact_energy = self.get_impact_energy(velocity)
+        return self.maxwell(v) * velocity * self.cross_section(impact_energy)
 
-class IonRateIntegrator2D(IonRateIntegrator):
     @staticmethod
-    def integrate(function):
+    def integrate_2d(function):
         return scipy.integrate.dblquad(function, 0, numpy.inf, -numpy.pi, numpy.pi)[0]
 
-    def integrand_normalisation(self, alpha, v):
+    def integrand_normalisation_2d(self, alpha, v):
         return self.maxwell(v)
 
-
-class IonRateIntegrator3D(IonRateIntegrator):
     @staticmethod
-    def integrate(function):
+    def integrate_3d(function):
         return scipy.integrate.tplquad(function, 0, numpy.inf, -numpy.pi, numpy.pi, -numpy.pi/2, numpy.pi/2)[0]
 
-    def integrand_normalisation(self, beta, alpha, v):
+    def integrand_normalisation_3d(self, beta, alpha, v):
         return self.maxwell(v)
--- bebim/rate.py
+++ bebim/rate.py
@@ -1,19 +1,12 @@
 import numpy
 from bebim.beam import Beam
 from bebim.cross_section import CrossSection
-from integrator import ElectronRateIntegrator1D, IonRateIntegrator2D, IonRateIntegrator3D
-from tabata_ctf.cross_section import CrossSection as CXCrossSection
+from integrator import RateIntegrator
 
 
 class Rate(Beam):
     def __init__(self, species, beam_energy, ionisation_level=0):
         super().__init__(species, beam_energy, ionisation_level)
-        self.beb = CrossSection(1000, self.species, self.ionisation_level)
-        self.beb.set_polynomial()
-        self.tabata = CXCrossSection(1000, self.species)
-        self.tabata.set_polynomial()
-        self.tabata_double = CXCrossSection(1000, self.species, degree='double')
-        self.tabata_double.set_polynomial()
 
     def set_profiles(self, electron_temperature=numpy.nan, electron_density=numpy.nan):
         if ~numpy.isnan(electron_temperature):
@@ -24,27 +17,22 @@ class Rate(Beam):
     def get_attenuation_nrl(self, is_with_tabata=False, tabata_integration_dimension=2):
         if self.electron_density == 0:
             return 0.0
-        c = CrossSection(self.electron_temperature, self.species, self.ionisation_level)
-        t = c.get_t()
+        c = CrossSection(self.species, self.ionisation_level)
+        t = c.get_t(self.electron_temperature)
         r = 1e-11 * numpy.sqrt(t) / c.B ** 1.5 / (6.0 + t) * numpy.exp(-1.0 / t)
         if is_with_tabata:
-            if tabata_integration_dimension == 2:
-                r += IonRateIntegrator2D(self.electron_temperature, self.speed, self.tabata.f, self.tabata_double.f).get_coefficient()
-            elif tabata_integration_dimension == 3:
-                r += IonRateIntegrator3D(self.electron_temperature, self.speed, self.tabata.f, self.tabata_double.f).get_coefficient()
-            else:
-                raise ValueError
+            r += RateIntegrator('charge exchange',
+                                self.species, self.speed, self.electron_temperature, tabata_integration_dimension)\
+                .get_coefficient()
         return r * self.electron_density
 
     def get_attenuation_beb(self, is_with_tabata=False, tabata_integration_dimension=2):
         if self.electron_density == 0:
             return 0.0
-        r = ElectronRateIntegrator1D(self.electron_temperature, self.beb.f).get_coefficient()
+        r = RateIntegrator('electron impact ionisation', self.species, self.speed, self.electron_temperature, 1)\
+            .get_coefficient()
         if is_with_tabata:
-            if tabata_integration_dimension == 2:
-                r += IonRateIntegrator2D(self.electron_temperature, self.speed, self.tabata.f, self.tabata_double.f).get_coefficient()
-            elif tabata_integration_dimension == 3:
-                r += IonRateIntegrator3D(self.electron_temperature, self.speed, self.tabata.f, self.tabata_double.f).get_coefficient()
-            else:
-                raise ValueError
+            r += RateIntegrator('charge exchange',
+                                self.species, self.speed, self.electron_temperature, tabata_integration_dimension)\
+                .get_coefficient()
         return r * self.electron_density
