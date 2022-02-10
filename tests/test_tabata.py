d146d699361b38c4a4db649a90701b78b2b1124b
Mátyás Aradi <20679946+matyasaradi@users.noreply.github.com>
2022-02-10 22:17:26 +0100

    #19 Fix test cases: add energy as the input parameter of calculate()

--- tests/test_tabata.py
+++ tests/test_tabata.py
@@ -11,11 +11,12 @@ class TestTabataCrossSection(unittest.TestCase):
     def test_broken_database(self):
         c = CrossSection('Li')
         c.tabata_data.__delitem__('Et')
-        self.assertRaises(KeyError, c.calculate)
+        self.assertRaises(KeyError, c.calculate, 1000)
 
     def test_low_energy(self):
+        energy = numpy.logspace(0, 6, 50)
         c = CrossSection('Na', 'double')
-        value = c.calculate(numpy.logspace(0, 6, 50))[0]
+        value = c.calculate(energy)[0]
         self.assertFalse(numpy.isnan(value))
 
 
