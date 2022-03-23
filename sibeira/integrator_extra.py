5879f2e0d5b7ea5af02b3878531f8d3ab3b0afa0
Mátyás Aradi <20679946+matyasaradi@users.noreply.github.com>
2022-03-23 22:36:36 +0100

    #28 fix Maxwell distribution

--- sibeira/integrator_extra.py
+++ sibeira/integrator_extra.py
@@ -6,15 +6,18 @@ import scipy.stats
 from sibeira.integrator import RateIntegrator
 
 
+def maxwell(m, T, v):
+    m__2kT = m / (2.0 * T * scipy.constants.elementary_charge)
+    return numpy.sqrt((m__2kT / numpy.pi) ** 3) * numpy.exp(- m__2kT * v ** 2)
+
+
 class RateIntegratorExtra(RateIntegrator):
-    @staticmethod
-    def maxwell_v2(m, T, v):
-        m__2kT = m / (2.0 * T * scipy.constants.elementary_charge)
-        return numpy.sqrt((m__2kT / numpy.pi) ** 3) * numpy.exp(- m__2kT * v ** 2)
+    def maxwell(self, velocity):
+        return maxwell(self.target_mass, self.temperature, velocity)
 
     def integrand_1d(self, velocity):
         impact_energy = self.get_impact_energy(velocity)
-        return self.maxwell_v2(velocity) * velocity * self.cross_section(impact_energy)
+        return self.maxwell(velocity) * velocity * self.cross_section(impact_energy)
 
-    def integrand_normalisation_1d(self, v):
-        return self.maxwell_v2(v)
+    def integrand_normalisation_1d(self, velocity):
+        return self.maxwell(velocity)
