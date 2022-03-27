0159a9a9a527f3acbd9992e5d26943451bd6a26a
Mátyás Aradi <20679946+matyasaradi@users.noreply.github.com>
2022-03-27 22:19:04 +0200

    #36 Add and fix scenario references and calls

--- example/test_renate_od.py
+++ example/test_renate_od.py
@@ -6,27 +6,27 @@ from renate_od import get_export_name, get_scenario_latex, get_scenario_path
 class TestRenateODExportName(unittest.TestCase):
     def test_export_name(self):
         print(get_export_name('plot', 12345, 678, 'test', 123))
-        print(get_export_name('log', 12345, 678, 'test', 123))
-        print(get_export_name('lorem_ipsum', 12345, 678, 'test', 123))
-        print(get_export_name('dolor_sit_amet', 12345, 678, 'test', 123, 3))
-        print(get_export_name('dolor_sit_amet', 12345, 678, 'test', 123, 3, 'just electron'))
+#        print(get_export_name('log', 12345, 678, 'test', 123))
+#        print(get_export_name('lorem_ipsum', 12345, 678, 'test', 123))
+#        print(get_export_name('dolor_sit_amet', 12345, 678, 'test', 123, 3))
+#        print(get_export_name('dolor_sit_amet', 12345, 678, 'test', 123, 3, 'just electron'))
 
 
 class TestRenateODScenarioLatex(unittest.TestCase):
     def test_total(self):
         reference = ''
-        self.assertEqual(get_scenario_path('total'), reference, 'Scenario LaTeX test for combnined models')
+        self.assertEqual(get_scenario_latex('total'), reference, 'Scenario LaTeX test for combnined models')
 
     def test_just_electron(self):
-        reference = ''
-        self.assertEqual(get_scenario_path('just electron'), reference, 'Scenario LaTeX test for pure electron models')
+        reference = ', $n_i = 0$'
+        self.assertEqual(get_scenario_latex('just electron'), reference, 'Scenario LaTeX test for pure electron models')
 
     def test_just_ion(self):
-        reference = ''
-        self.assertEqual(get_scenario_path('just electron'), reference, 'Scenario LaTeX test for pure ion models')
+        reference = ', $n_e = 0$'
+        self.assertEqual(get_scenario_latex('just ion'), reference, 'Scenario LaTeX test for pure ion models')
 
     def test_invalid(self):
-        self.assertRaises(ValueError, get_scenario_path, 'Scenario LaTeX test invalid case')
+        self.assertRaises(ValueError, get_scenario_latex, 'Scenario LaTeX test invalid case')
 
 
 class TestRenateODScenarioPath(unittest.TestCase):
@@ -35,12 +35,12 @@ class TestRenateODScenarioPath(unittest.TestCase):
         self.assertEqual(get_scenario_path('total'), reference, 'Scenario path test for combnined models')
 
     def test_just_electron(self):
-        reference = ''
+        reference = '_electron'
         self.assertEqual(get_scenario_path('just electron'), reference, 'Scenario path test for pure electron models')
 
     def test_just_ion(self):
-        reference = ''
-        self.assertEqual(get_scenario_path('just electron'), reference, 'Scenario path test for pure ion models')
+        reference = '_ion'
+        self.assertEqual(get_scenario_path('just ion'), reference, 'Scenario path test for pure ion models')
 
     def test_invalid(self):
         self.assertRaises(ValueError, get_scenario_path, 'Scenario path test invalid case')
