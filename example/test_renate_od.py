09bd35505406c392f36adf10b0de38f5e5a26a7b
Mátyás Aradi <20679946+matyasaradi@users.noreply.github.com>
2022-03-27 22:12:01 +0200

    #36 Add scenario path test

--- example/test_renate_od.py
+++ example/test_renate_od.py
@@ -3,7 +3,7 @@ import unittest
 from renate_od import get_export_name, get_scenario_latex, get_scenario_path
 
 
-class TestRenateOD(unittest.TestCase):
+class TestRenateODExportName(unittest.TestCase):
     def test_export_name(self):
         print(get_export_name('plot', 12345, 678, 'test', 123))
         print(get_export_name('log', 12345, 678, 'test', 123))
@@ -11,16 +11,29 @@ class TestRenateOD(unittest.TestCase):
         print(get_export_name('dolor_sit_amet', 12345, 678, 'test', 123, 3))
         print(get_export_name('dolor_sit_amet', 12345, 678, 'test', 123, 3, 'just electron'))
 
+
+class TestRenateODScenarioLatex(unittest.TestCase):
     def test_scenario_latex(self):
         print(get_scenario_latex('total'))
         print(get_scenario_latex('just electron'))
         print(get_scenario_latex('just ion'))
         self.assertRaises(ValueError, get_scenario_latex, 'fake scenario')
 
-    def test_scenario_path(self):
-        print(get_scenario_path('total'))
-        print(get_scenario_path('just electron'))
-        print(get_scenario_path('just ion'))
+
+class TestRenateODScenarioPath(unittest.TestCase):
+    def test_total(self):
+        reference = ''
+        self.assertEqual(get_scenario_path('total'), reference, 'Scenario path test for combnined models')
+
+    def test_just_electron(self):
+        reference = ''
+        self.assertEqual(get_scenario_path('just electron'), reference, 'Scenario path test for pure electron models')
+
+    def test_just_ion(self):
+        reference = ''
+        self.assertEqual(get_scenario_path('just electron'), reference, 'Scenario path test for pure ion models')
+
+    def test_invalid(self):
         self.assertRaises(ValueError, get_scenario_path, 'fake scenario')
 
 
