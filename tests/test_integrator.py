cf68ed4566a58bd9a9e9466b1ae9a7dea7c35e18
Mátyás Aradi <20679946+matyasaradi@users.noreply.github.com>
2022-04-20 20:54:51 +0200

    #58 Add speed mean test

--- tests/test_integrator.py
+++ tests/test_integrator.py
@@ -25,6 +25,8 @@ class TestIntegrator(unittest.TestCase):
     def test_target_mass_invalid(self):
         self.assertRaises(ValueError, RateIntegrator, 'charge exchange', 'unknown species', 0, 0)
 
+
+class TestIntegratorNormalisation(unittest.TestCase):
     def test_normalisation_1d(self):
         rate = RateIntegrator('electron impact ionisation', 'Li', 40, 100, 1)
         normalisation_factor = rate.integrate(rate.integrand_normalisation)
@@ -37,11 +39,37 @@ class TestIntegrator(unittest.TestCase):
         numpy.testing.assert_array_almost_equal(normalisation_factor, 2.0*scipy.constants.pi, decimal=4,
                                                 err_msg='2D normalisation factor')
 
-    def test_normalisation_3d(self):
-        rate = RateIntegrator('charge exchange', 'Li', 40, 100, 3)
-        normalisation_factor = rate.integrate(rate.integrand_normalisation)
-        numpy.testing.assert_array_almost_equal(normalisation_factor, 2.0*scipy.constants.pi**2, decimal=4,
-                                                err_msg='3D normalisation factor')
+
+class TestIntegratorSpeed(unittest.TestCase):
+    @staticmethod
+    def one(x):
+        return 1
+
+    @staticmethod
+    def identity(x):
+        return x
+
+    @staticmethod
+    def square(x):
+        return x**2
+
+    def test_mean_speed_1d(self):
+        temperature = 100
+        rate = RateIntegrator('electron impact ionisation', 'Li', 40, temperature, 1)
+        rate.cross_section = self.one
+        mean_speed = rate.get_coefficient()
+        expected = numpy.sqrt(8.0 * scipy.constants.e * temperature / scipy.constants.pi / scipy.constants.electron_mass)
+        numpy.testing.assert_approx_equal(mean_speed, expected, significant=7,
+                                          err_msg='1D normalisation factor')
+
+    def test_mean_speed_2d(self):
+        temperature = 100
+        rate = RateIntegrator('electron impact ionisation', 'Li', 40, temperature, 2)
+        rate.cross_section = self.one
+        mean_speed = rate.get_coefficient()
+        expected = numpy.sqrt(8.0 * scipy.constants.e * temperature / scipy.constants.pi / scipy.constants.electron_mass)
+        numpy.testing.assert_approx_equal(mean_speed, expected, significant=5,
+                                          err_msg='1D normalisation factor')
 
 
 if __name__ == '__main__':
